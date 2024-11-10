aaff5cf022a454c83d03772a54e42984e442c838
Hina-Aamir <hinaaamir9911@gmail.com>
2024-11-10 19:21:13 +0500

    second try

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,8 @@ export default function Home() {
     <div >
          <Header />
          <div className="flex">
-         <div className="container h-[calc(100vh-100px)] ,w-1/2 bg-white p-20 ">
-          <h1 className="text-extrabold text-3xl
-
-">IMPECCABLE <br></br>CRAFTSMANSHIP AND <br></br>FINESSE
+         <div className="container bg-blue h-[calc(100vh-100px)] ,w-1/2 bg-white p-20 ">
+          <h1 className="text-extrabold text-3xl">IMPECCABLE <br></br>CRAFTSMANSHIP AND <br></br>FINESSE
          </h1><br></br><br></br>
           <p>An example of intricate workmanship and detail, elegant<br></br> 
           necklaces and long and short chains form a part of our <br></br>
@@ -20,14 +18,11 @@ export default function Home() {
   EXPLORE NOW
 </button>
 
-
-
-
            </div>
      
-<div className="container h-[calc(100vh-100px)] ,w-full bg-white p-10 ">
+<div className="container bg-pink h-[calc(100vh-100px)] ,w-full bg-white p-10 ">
 <img src="assets/img1.png" alt="Image" className="w-[450px] h-[500px] " />
-{/* <img src="assets/img2.png" alt="Image" className="w-[400px] h-[459px] m-8" /> */}
+
 </div>
 
 </div> 
