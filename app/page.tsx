553e5e37468ee48edaebc21fc1d02392b7f4bf5c
Hina-Aamir <hinaaamir9911@gmail.com>
2024-11-10 19:26:32 +0500

    third try

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Header from "./components/Header/header.tsx";
+import Header from "./components/Header/header";
 
 export default function Home() {
   return (
